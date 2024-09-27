3977a56644f719b580b160bcb6aab9090f4c5cf5
Yvonne Bosire <yvonnebosire24@gmail.com>
2024-09-27 12:36:01 +0300

    added changes to allow user input city

--- weather.py
+++ weather.py
@@ -1,22 +1,20 @@
-import requests  # Import the requests library
+import requests  
 
-# Replace 'your_api_key' with your actual API key from WeatherAPI
+
 API_KEY = '87029d5cf2524294bae91802242709'
 BASE_URL = 'http://api.weatherapi.com/v1/current.json'
 
-# Specify the city for which you want to get weather information
-city = 'Nairobi'
+city = input("Enter the name of the city: ")
 
-# Construct the API request URL
+
 url = f"{BASE_URL}?key={API_KEY}&q={city}"
 
-# Fetch weather data using requests.get() method
 response = requests.get(url)
 
-# Parse the JSON response
+
 data = response.json()
 
-# Extract relevant weather information
+
 if response.status_code == 200:
     temperature = data['current']['temp_c']
     condition = data['current']['condition']['text']
